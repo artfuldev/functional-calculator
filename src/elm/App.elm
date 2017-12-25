c3d7a66c2eaad0011d8cad5f9b11c87530b61e60
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2017-12-26 00:27:39 +0530

    Restrict input

--- src/elm/App.elm
+++ src/elm/App.elm
@@ -2,7 +2,7 @@ module App exposing (..)
 
 import Html exposing (..)
 import Html.Attributes exposing (..)
-import Html.Events exposing (..)
+import Events exposing (onKeyDown)
 
 import Calculation exposing (parser, perform)
 import Parser exposing (run)
@@ -23,7 +23,34 @@ model =
   }
 
 type Msg
-  = ExpressionInputChanged String
+  = KeyPressed String
+
+removeLastCharacter: String -> String
+removeLastCharacter = String.slice 0 -1
+
+expression: String -> String -> String
+expression previous current =
+  case current of
+    "1" -> previous ++ current
+    "2" -> previous ++ current
+    "3" -> previous ++ current
+    "4" -> previous ++ current
+    "5" -> previous ++ current
+    "6" -> previous ++ current
+    "7" -> previous ++ current
+    "8" -> previous ++ current
+    "9" -> previous ++ current
+    "0" -> previous ++ current
+    "+" -> previous ++ current
+    "-" -> previous ++ current
+    "*" -> previous ++ current
+    "/" -> previous ++ current
+    "." -> previous ++ current
+    "e" -> previous ++ current
+    "E" -> previous ++ current
+    "Backspace" -> removeLastCharacter previous
+    "Delete" -> removeLastCharacter previous
+    _ -> previous
 
 result : String -> Maybe Float
 result value =
@@ -34,20 +61,21 @@ result value =
 update : Msg -> Model -> Model
 update msg model =
   case msg of
-    ExpressionInputChanged value -> { model | expression = value, result = result value }
+    KeyPressed value ->
+      let expr = expression model.expression value
+      in { model | expression = expr, result = result <| expr }
 
 toResultString : Maybe Float -> String
 toResultString value =
   case value of
     Just x -> toString x
     Nothing -> ""
-    
 
 view : Model -> Html Msg
 view model =
   div [ id "app" ]
     [ header []
-      [ input [ placeholder "0", onInput ExpressionInputChanged ] []
+      [ input [ placeholder "0", onKeyDown KeyPressed, value model.expression ] []
       , p [] [ text <| toResultString model.result ]
       ]
     , main_ [] []  
