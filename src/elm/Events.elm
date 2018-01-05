2c9cae8e64b07e0696878c8bff7258c1d1bdc24b
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2018-01-05 23:13:20 +0530

    Apply elm-format formatting

--- src/elm/Events.elm
+++ src/elm/Events.elm
@@ -4,16 +4,16 @@ import Html exposing (Attribute)
 import Html.Events exposing (onWithOptions, Options, keyCode)
 import Json.Decode as Json exposing (field, string, map)
 
-eventOptions: Options
+eventOptions : Options
 eventOptions = 
   { stopPropagation = True
   , preventDefault = True
   }
 
-key: Json.Decoder String
+key : Json.Decoder String
 key =
   field "key" string
 
-onKeyDown: (String -> msg) -> Attribute msg
+onKeyDown : (String -> msg) -> Attribute msg
 onKeyDown handler =
   onWithOptions "keydown" eventOptions <| map handler key
