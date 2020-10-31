faee1ab028e90657d7b6bc8c2f5b1c3bd6592e1b
Sudarsan Balaji <sudarsan@rocketlane.com>
2020-10-31 22:30:25 +0530

    Use a custom number type

--- src/Calculation.elm
+++ src/Calculation.elm
@@ -1,16 +1,16 @@
 module Calculation exposing (parser, perform)
 
 import Operation exposing (Operation, operate)
-import Number
+import Number exposing (Number)
 import Parser exposing (Parser, (|.), (|=), succeed, float, end, run)
 
 type alias Calculation =
-  { left: Float
+  { left: Number
   , operation: Operation
-  , right: Float
+  , right:  Number
   }
 
-perform: Calculation -> Float
+perform: Calculation -> Number
 perform { left, operation, right } =
   operate operation left right 
 
--- src/Number.elm
+++ src/Number.elm
@@ -1,9 +1,83 @@
-module Number exposing (parser)
+module Number exposing (parser, Number, add, subtract, multiply, divide, toString)
 
+import Basics exposing (Bool, Int)
 import Parser exposing (Parser, (|.), (|=), succeed, float, oneOf, map, symbol, andThen)
+import Flip exposing (flip)
+import Svg.Attributes exposing (exponent)
+import Parser exposing (number)
+import String exposing (fromFloat)
 
--- The implementation is a simplified version adapted from https://github.com/zwilias/elm-json
-parser : Parser Float
+type alias Number =
+  { significand: Int
+  , exponent: Int
+  }
+
+normalize: Number -> Number
+normalize number =
+  if (number.significand == 0 || number.exponent == 0 || Basics.remainderBy 10 number.significand /= 0)
+  then number
+  else
+    normalize
+      { significand = number.significand // 10
+      , exponent = number.exponent - 1
+      }
+
+toString: Number -> String
+toString number =
+  if number.exponent == 0
+  then number.significand |> String.fromInt
+  else
+    let significand = number.significand |> String.fromInt |> String.padLeft -number.exponent '0' in
+    let characteristic = significand |> String.slice 0 number.exponent in
+    let mantissa = significand |> String.right -number.exponent in
+    characteristic ++ "." ++ mantissa
+
+withExponent: Int -> Number -> Number
+withExponent exponent number  =
+  let final = min number.exponent exponent
+  in
+    if final >= number.exponent
+    then number
+    else
+      { significand = number.significand * (10 ^ -(final - number.exponent))
+      , exponent = final
+      }
+
+add: Number -> Number -> Number
+add x y =
+  let exponent = min x.exponent y.exponent in
+  let normalizedWithExponent = exponent |> withExponent in
+  let a = normalizedWithExponent x in
+  let b = normalizedWithExponent y in
+    normalize
+      { significand = a.significand + b.significand
+      , exponent = exponent
+      }
+
+subtract: Number -> Number -> Number
+subtract x y =
+  let exponent = min x.exponent y.exponent in
+  let normalizedWithExponent = exponent |> withExponent in
+  let a = normalizedWithExponent x in
+  let b = normalizedWithExponent y in
+    normalize
+      { significand = a.significand - b.significand
+      , exponent = exponent
+      }
+
+multiply: Number -> Number -> Number
+multiply x y =
+  normalize
+    { significand = x.significand * y.significand
+    , exponent = x.exponent + y.exponent
+    }
+
+divide: Number -> Number -> Number
+divide x y =
+  let result = (Basics.toFloat x.significand) / (Basics.toFloat y.significand) |> fromFloat in
+  normalize result
+
+parser : Parser Number
 parser =
   succeed applySign
     |= maybeNegative
@@ -19,25 +93,6 @@ maybeNegative =
 parseNumber : Parser Float
 parseNumber =
     float
-      |> andThen maybeExponentiate
-
-maybeExponentiate : Float -> Parser Float
-maybeExponentiate number =
-  oneOf
-    [ exponent |> map (applyExponent number)
-    , succeed number
-    ]
-
-exponent : Parser Float
-exponent =
-  succeed applySign
-    |. oneOf [ symbol "e", symbol "E" ]
-    |= sign
-    |= float
-
-applyExponent : Float -> Float -> Float
-applyExponent coefficient e =
-  coefficient * (10 ^ e)
 
 type Sign
   = Positive
@@ -51,8 +106,14 @@ sign =
     , succeed Positive
     ]
 
-applySign : Sign -> number -> number
+fromFloat: Float -> Number
+fromFloat number =
+  { significand = number |> String.fromFloat |> String.replace "." "" |> String.toInt |> Maybe.withDefault 0
+  , exponent = -(number |> String.fromFloat |> String.split "." |> List.tail |> Maybe.withDefault [] |> List.head |> Maybe.withDefault "" |> String.length)
+  }
+
+applySign : Sign -> Float -> Number
 applySign s number =
   case s of
-    Positive -> number
-    Negative -> -number
+    Positive -> fromFloat number      
+    Negative -> fromFloat -number
